eeb5d905460a47a4b678d74a0dbe6fbd719751d9
Chris Thiele <chris.thiele@haw-hamburg.de>
2021-04-07 23:39:13 +0200

    ct, moved UserDetailsService, .sql file changes

--- sql/FILL_TABLEAS_MANDATORY.sql
+++ sql/FILL_TABLEAS_MANDATORY.sql
@@ -6,10 +6,6 @@ insert into "dormitories"
 ("dormitory_id", "name", "address_id") values
 (0, 'Gustav-Radbruch-Haus', 0);
 
-insert into "users"
-("user_id", "email", "full_name", "dormitory_id", "flat", "password_hash", "salt", "enabled", "comment") values
-(0, 'chris.23.thiele@gmail.com', 'Chris Thiele', 0, null, '1234', '1234', true, 'What the heck im doing?');
-
 insert into "roles"
 ("role_id", "name") values
 (0, 'ADMIN'),
@@ -17,10 +13,3 @@ insert into "roles"
 (2, 'SHC-MEMBER'),
 (3, 'RESIDENT'),
 (4, 'GUEST');
-
-insert into "users_roles"
-("user_id", "role_id") values
-(0, 0),
-(0, 1),
-(0, 2),
-(0, 3);
---
+++ sql/dev/FILL_TABLES_DEV.sql
@@ -0,0 +1,27 @@
+insert into "addresses"
+("address_id", "street", "street_number", "postal_code", "city", "country_iso", "comment") values
+(0, 'Borgfelder Str.', 16, '20537', 'Hamburg', 'DE', '');
+
+insert into "dormitories"
+("dormitory_id", "name", "address_id") values
+(0, 'Gustav-Radbruch-Haus', 0);
+
+insert into "users"
+("user_id", "email", "full_name", "dormitory_id", "flat", "password_hash", "salt", "enabled", "comment") values
+(0, 'chris.23.thiele@gmail.com', 'Chris Thiele', 0, null, '1234', '1234', true, 'What the heck im doing?'),
+(1, 'thiele.23.chris@gmail.com', 'Thiele Chris', 0, null, '4321', '4321', true, 'Doing im heck the what?');
+
+insert into "roles"
+("role_id", "name") values
+(0, 'ADMIN'),
+(1, 'DORM-ADMIN'),
+(2, 'SHC-MEMBER'),
+(3, 'RESIDENT'),
+(4, 'GUEST');
+
+insert into "users_roles"
+("user_id", "role_id") values
+(0, 0),
+(0, 1),
+(0, 2),
+(0, 3);
