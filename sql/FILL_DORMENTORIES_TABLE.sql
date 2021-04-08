aa8b68afd51079adb202abeb9881a7fb128ebfe1
Chris Thiele <chris.thiele@haw-hamburg.de>
2021-04-08 23:03:06 +0200

    ct, fixed sql typo and added dormentories.sql

---
+++ sql/FILL_DORMENTORIES_TABLE.sql
@@ -0,0 +1,7 @@
+insert into "addresses"
+("address_id", "street", "street_number", "postal_code", "city", "country_iso", "comment") values
+(0, '', '', '', '', '', '');
+
+insert into "dormitories"
+("dormitory_id", "name", "address_id") values
+(0, '', 0);
