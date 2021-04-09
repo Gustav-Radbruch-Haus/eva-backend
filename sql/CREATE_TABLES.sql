53da29ee71c3fd2800ba93a5e8784f13bce139d4
Chris Thiele <chris.thiele@haw-hamburg.de>
2021-04-09 20:44:10 +0200

    ct, added unique'ness for users_roles relations

--- sql/CREATE_TABLES.sql
+++ sql/CREATE_TABLES.sql
@@ -36,6 +36,7 @@ create table "roles" (
 create table "users_roles" (
   "user_id" int not null,
   "role_id" int not null,
+  unique ("user_id", "role_id"),
   constraint "role_fk" foreign key ("role_id") references "roles" ("role_id"),
   constraint "user_fk" foreign key ("user_id") references "users" ("user_id")
 );
